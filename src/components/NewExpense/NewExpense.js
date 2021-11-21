2993194cbb63e0abc49c0efa67a4662c41f20ea3
Daniel Moreto <danifim@br.ibm.com>
2021-11-21 13:10:06 -0300

    Assignment #4

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,43 @@
+import { useState } from "react"
+
 import ExpenseForm from "./ExpenseForm"
+
 import "./NewExpense.css"
 
 const NewExpense = (props) => {
+  const [openedForm, setOpenedForm] = useState(false)
+
+  const openFormHandler = () => {
+    setOpenedForm(true)
+  }
+
+  const closeFormHandler = () => {
+    setOpenedForm(false)
+  }
+
   const saveExpanseDataHandler = enteredExpenseData => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     }
     props.onAddExpense(expenseData)
+    closeFormHandler()
+  }
+
+  if(!openedForm) {
+    return (
+      <div className="new-expense">
+        <button type="submit" onClick={openFormHandler}>Add Expense</button>
+      </div>
+    )
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpanseDataHandler} />
+      <ExpenseForm
+        onSaveExpenseData={saveExpanseDataHandler}
+        onCancel={closeFormHandler}
+      />
     </div>
   )
 }
